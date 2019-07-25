053a2e5efb10eab9de94885ae370f907bf2c8e52
csrunner <chaoshi_cn@hotmail.com>
2019-07-25 20:39:47 +0800

    P:1078

---
+++ 1078_occurrences_after_bigram.py
@@ -0,0 +1,43 @@
+Given words first and second, consider occurrences in some text of the form "first second third", where second comes immediately after first, and third comes immediately after second.
+
+For each such occurrence, add "third" to the answer, and return the answer.
+
+ 
+
+Example 1:
+
+Input: text = "alice is a good girl she is a good student", first = "a", second = "good"
+Output: ["girl","student"]
+Example 2:
+
+Input: text = "we will we will rock you", first = "we", second = "will"
+Output: ["we","rock"]
+ 
+
+Note:
+
+1 <= text.length <= 1000
+text consists of space separated words, where each word consists of lowercase English letters.
+1 <= first.length, second.length <= 10
+first and second consist of lowercase English letters.
+'''
+class Solution(object):
+    def findOcurrences(self, text, first, second):
+        """
+        :type text: str
+        :type first: str
+        :type second: str
+        :rtype: List[str]
+        """
+        text_list = text.split(' ')
+        
+        output = []
+        if len(text_list) < 3:
+            return []
+        for i in range(len(text_list)-1):
+            if text_list[i] == first and text_list[i+1] == second:
+                try:
+                    output.append(str(text_list[i+2]))
+                except IndexError:
+                    continue
+        return output
